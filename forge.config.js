a6c7cbbe0a409e78dbd368c18bf7c42912bc0202
ulkuehn <qgit@gmx.net>
2024-03-08 22:40:18 +0100

    ok

---
+++ forge.config.js
@@ -0,0 +1,65 @@
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        loadingGif: "dim.gif",
+        // authors: "_DIM",
+        // owners: "ownedby...",
+        // description: "describethisnot",
+        // version: "3.4.5", // Version in der App-Übersicht der Einstellungen
+        // title: "theTitle", // wird zum Titel in der App-Übersicht der Einstellungen
+        // name: "theName"
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {},
+    },
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "ulkuehn",
+          name: "DIM",
+        },
+        authToken:"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",
+        prerelease: true,
+      },
+    },
+  ],
+  plugins: [
+    {
+      name: "@electron-forge/plugin-auto-unpack-natives",
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
